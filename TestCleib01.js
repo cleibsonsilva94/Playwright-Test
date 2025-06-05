f5815e3fd566902360095f9e7cfa67c24b0af4bc
Cleibson Silva <cleibsonslima@gmail.com>
2025-06-05 11:37:11 -0300

    Formatting

--- TestCleib01.js
+++ TestCleib01.js
@@ -1,57 +1,61 @@
 const { chromium } = require('playwright');
 const expect = require('expect');
+
 // -- CT001 Validating the researcher -- //
+/*
+(async () => {
+  const browser = await chromium.launch({ headless: false, slowMo: 90 });
+  const context = await browser.newContext();
+  const page = await context.newPage();
 
-/*(async () => {
-    const browser = await chromium.launch({ headless: false, slowMo: 90 })
-    const context = await browser.newContext();
-    const page = await context.newPage();
+  await page.goto('https://www.jw.org/en/');
+  await page.fill('input[type="text"]', 'Dead');
+  await page.press('input[type="text"]', 'Enter');
+  await page.waitForTimeout(5000);
 
-    await page.goto('https://www.jw.org/en/');
-    await page.fill('input[type="text"]', 'Dead');
-    await page.press('input[type="text"]', 'Enter');
-    await page.waitForTimeout(5000)
-    await browser.close();
-})()*/
+  await browser.close();
+})();
+*/
 
 // -- CT002 Validating research results -- //
-
 (async () => {
-    const browser = await chromium.launch({ headless: false, slowMo: 90 });
-    const context = await browser.newContext();
-    const page = await context.newPage();
+  const browser = await chromium.launch({ headless: false, slowMo: 90 });
+  const context = await browser.newContext();
+  const page = await context.newPage();
 
-    await page.goto('https://www.jw.org/en/');
-    await page.fill('input[type="text"]', 'Dead');
-    await page.press('input[type="text"]', 'Enter');
-    await page.waitForTimeout(5000);
+  await page.goto('https://www.jw.org/en/');
+  await page.fill('input[type="text"]', 'Dead');
+  await page.press('input[type="text"]', 'Enter');
+  await page.waitForTimeout(5000);
 
-    const result = await page.locator('text=What Hope for the Dead?').first().isVisible();
-    expect(result).toBe(true);
+  const result = await page.locator('text=What Hope for the Dead?').first().isVisible();
+  expect(result).toBe(true);
 
-    await browser.close();
+  await browser.close();
 })();
 
 // -- CT002 Validating search with if -- //
-/*(async () => {
-    const browser = await chromium.launch({ headless: false, slowMo: 90 });
-    const context = await browser.newContext();
-    const page = await context.newPage();
+/*
+(async () => {
+  const browser = await chromium.launch({ headless: false, slowMo: 90 });
+  const context = await browser.newContext();
+  const page = await context.newPage();
 
-    await page.goto('https://www.jw.org/en/');
-    await page.fill('input[type="text"]', 'Dead');
-    await page.press('input[type="text"]', 'Enter');
+  await page.goto('https://www.jw.org/en/');
+  await page.fill('input[type="text"]', 'Dead');
+  await page.press('input[type="text"]', 'Enter');
 
-    await page.waitForTimeout(5000);
+  await page.waitForTimeout(5000);
 
-    const result = await page.locator('text=What Hope for the Dead?').first().isVisible();
+  const result = await page.locator('text=What Hope for the Dead?').first().isVisible();
 
-    if (result) {
-        console.log('✅ Resultado encontrado: O item "What Hope for the Dead?" está visível.');
-    } else {
-        console.error('❌ Resultado não encontrado: O item "What Hope for the Dead?" não está visível.');
-        process.exit(1); // para encerrar com erro se estiver em CI/CD
-    }
+  if (result) {
+    console.log('Resultado encontrado: O item "What Hope for the Dead?" está visível.');
+  } else {
+    console.error('Resultado não encontrado: O item "What Hope for the Dead?" não está visível.');
+    process.exit(1); // para encerrar com erro se estiver em CI/CD
+  }
 
-    await browser.close();
-})()*/
+  await browser.close();
+})();
+*/
