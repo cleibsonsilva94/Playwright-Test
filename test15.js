3af857a3ed2242b3b2feeb89ea03ad5aed57da09
Cleibson Silva <cleibsonslima@gmail.com>
2025-06-09 16:01:45 -0300

    Adding time zone test

---
+++ test15.js
@@ -0,0 +1,14 @@
+// -- TESTE DE FUSO HÓRARIO -- // 
+
+const { chromium } = require('playwright');
+const expect = require('expect');
+
+(async () => {
+  const browser = await chromium.launch({headless: false})
+  const context = await browser.newContext({
+      locale: 'de-DE',
+      timezoneId: 'Europe/Berlin',
+  })
+  const page = await context.newPage()
+  await page.goto('http://www.thetimezoneconverter.com/')
+})()
