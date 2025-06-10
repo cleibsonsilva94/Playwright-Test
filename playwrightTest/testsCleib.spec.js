f3545eb0f0e80eeac750d8b74d25f1b07c1a58cf
Cleibson Silva <cleibsonslima@gmail.com>
2025-06-10 12:01:44 -0300

    Adding your own tests using @playwright/test. Note: The second test was created using the recording robot.

--- playwrightTest/testsCleib.spec.js
+++ playwrightTest/testsCleib.spec.js
@@ -7,8 +7,6 @@ test.beforeEach(async ({ page }) => {
 test.describe('Grupo B JW', () => {
 
     test('Downloading magazine', async ({ page }) => {
-        await expect(page).toHaveTitle('Conduit');
-        await page.waitForLoadState();
         await page.fill('input[type="text"]', 'Dead');
         await page.press('input[type="text"]', 'Enter');
         await page.waitForTimeout(5000);
@@ -18,17 +16,27 @@ test.describe('Grupo B JW', () => {
         await magazinePdf.click();
         await page.waitForTimeout(3000);
     });
+});
 
+test.describe('Grupo C JW Donations', () => {
     // Outro teste de login (sem captura de tela)
-    test('login test', async ({ page }) => {
-        await expect(page).toHaveTitle('Conduit');
-
-        await page.fill('input[type="email"]', 'alanvoigt@yahoo.com.br');
-        await page.press('input[type="email"]', 'Tab');
-        await page.type('input[type="password"]', 'test123');
-        await page.click('form >> "Sign in"');
-
-        const locator = page.locator('.navbar-brand');
-        await expect(locator).toContainText('conduit', { timeout: 30000 });
+    test('Donations', async ({ page }) => {
+        await page.goto('https://www.jw.org/en/');
+        const page1Promise = page.waitForEvent('popup');
+        await page.getByRole('link', { name: 'Donations (opens new window)' }).click();
+        const page1 = await page1Promise;
+        await page1.goto('https://donate.jw.org/en/BRA/home');
+        await page1.getByRole('link', { name: 'Your Congregation' }).click();
+        await page1.getByRole('button', { name: 'Find Your Congregation' }).click();
+        await page1.getByRole('combobox', { name: 'Congregation' }).click();
+        await page1.getByRole('combobox', { name: 'Congregation' }).press('CapsLock');
+        await page1.getByRole('combobox', { name: 'Congregation' }).fill('Iputinga');
+        await page1.getByText('Iputinga - Recife PE').click();
+        await page1.locator('div').filter({ hasText: /^Iputinga - Recife PE$/ }).nth(1).click();
+        await page1.getByRole('definition').filter({ hasText: 'Est. do Barbalho, 249' }).click();
+        await page1.getByText('Iputinga - Recife PE').click();
+        await page1.getByTestId('ptrn-modal-content').getByRole('button', { name: 'Select' }).click();
+        await page1.getByRole('textbox', { name: 'Amount (BRL)' }).fill('2005118181');
+        await page.screenshot({ path: 'SignIn.png', fullPage: true })
     });
 });
