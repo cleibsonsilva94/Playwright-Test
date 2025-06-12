e0412b9a4a883bf062672d3643aaed392dc6770a
Cleibson Silva <cleibsonslima@gmail.com>
2025-06-12 11:25:15 -0300

    First corrected test 1.2

--- Playwright-Test/Playwright_Jest/__test__/TC01.js
+++ Playwright-Test/Playwright_Jest/__test__/TC01.js
@@ -1,15 +1,13 @@
 describe('Post', () => {
 	test('Sign In', async () => {
         await page.goto('https://www.saucedemo.com/')
-        const title = await page.title()
-        expect(title).toBe('Swag Labs')
-      
-        await page.fill('input[type = "email"]', 'standard_user')
-		    await page.press('input[type = "email"]', 'Tsecret_sauce')
-		    await page.type('input[type = "password"]', 'test123')
+
+        await page.fill('input[id="user-name"]', 'standard_user')
+        await page.fill('input[type="password"]', 'secr_sauce')
+
         await Promise.all([
           page.waitForNavigation(), 
-          await page.click('form >> "Sign in"') 
-        ]);
+          page.click('input[type="submit"]') 
+        ])
     })
 })
