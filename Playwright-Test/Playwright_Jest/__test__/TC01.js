0cc97f58e426e3a8e74749149b8b6232f8576e9e
Cleibson Silva <cleibsonslima@gmail.com>
2025-06-12 10:56:07 -0300

    First corrected test

--- Playwright-Test/Playwright_Jest/__test__/TC01.js
+++ Playwright-Test/Playwright_Jest/__test__/TC01.js
@@ -1,34 +1,15 @@
-// -- PRIMEIRO EXEMPLO DE TESTES COM JEST E PLAYWRIGHT JUNTOS -- //
-
-const { chromium } = require('playwright');
-
-describe('Post', () => { // Suite de testes
-  let browser;
-  let context;
-  let page;
-
-  beforeAll(async () => { // Configuração do navegador e da página
-    browser = await chromium.launch({ headless: true });
-    context = await browser.newContext();
-    page = await context.newPage();
-  });
-
-  afterAll(async () => { // Fechamento do navegador após os testes
-    await browser.close();
-  });
-
-  test('Sign In', async () => {
-    await page.goto('https://react-redux.realworld.io/#/login');
-    const title = await page.title();
-    expect(title).toBe('Conduit');
-
-    await page.fill('input[type="email"]', 'alanvoigt@yahoo.com.br');
-    await page.press('input[type="email"]', 'Tab');
-    await page.type('input[type="password"]', 'test123');
-    
-    await page.click('form >> text=Sign in');
-
-    await page.waitForSelector('a[href="#/editor"]', { timeout: 30000 }); 
-  }, 80000);
-
-});
+describe('Post', () => {
+	test('Sign In', async () => {
+        await page.goto('https://www.saucedemo.com/')
+        const title = await page.title()
+        expect(title).toBe('Swag Labs')
+      
+        await page.fill('input[type = "email"]', 'standard_user')
+		    await page.press('input[type = "email"]', 'Tsecret_sauce')
+		    await page.type('input[type = "password"]', 'test123')
+        await Promise.all([
+          page.waitForNavigation(), 
+          await page.click('form >> "Sign in"') 
+        ]);
+    })
+})
