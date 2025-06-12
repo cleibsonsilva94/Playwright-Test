5d37e29e716ea1b42beb3751a58362341d90cb2f
Cleibson Silva <cleibsonslima@gmail.com>
2025-06-12 11:41:15 -0300

    Test fix

--- Playwright-Test/Playwright_Jest/__test__/TC01.js
+++ Playwright-Test/Playwright_Jest/__test__/TC01.js
@@ -3,7 +3,7 @@ describe('Post', () => {
         await page.goto('https://www.saucedemo.com/')
 
         await page.fill('input[id="user-name"]', 'standard_user')
-        await page.fill('input[type="password"]', 'secr_sauce')
+        await page.fill('input[type="password"]', 'secret_sauce')
 
         await Promise.all([
           page.waitForNavigation(), 
