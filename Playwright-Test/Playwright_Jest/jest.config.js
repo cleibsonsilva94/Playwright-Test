671f632ee3cca73afab7c12c4321195eb1018339
Cleibson Silva <cleibsonslima@gmail.com>
2025-06-13 10:18:43 -0300

    Adding report generation code. Jest Reports

--- Playwright-Test/Playwright_Jest/jest.config.js
+++ Playwright-Test/Playwright_Jest/jest.config.js
@@ -1,5 +1,13 @@
 module.exports = {
   preset: 'jest-playwright-preset',
   testMatch: ['**/__test__/**/*.js'],
-  testEnvironment: './CustomEnvironment.js', // <- Caminho para seu ambiente customizado
-}
+  testEnvironment: './CustomEnvironment.js',
+  reporters: [
+    'default',
+    ['jest-html-reporters', {
+      publicPath: './html-report',
+      filename: 'report.html',
+      expand: true
+    }]
+  ]
+};
