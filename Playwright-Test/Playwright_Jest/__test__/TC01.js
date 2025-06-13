54144b99832850c94dc1ed9c17cd077b095bdcfc
Cleibson Silva <cleibsonslima@gmail.com>
2025-06-13 11:37:06 -0300

    Dta json

--- Playwright-Test/Playwright_Jest/__test__/TC01.js
+++ Playwright-Test/Playwright_Jest/__test__/TC01.js
@@ -1,9 +1,11 @@
+data = require('../data/data.json');
+
 describe('Post', () => {
 	test('Sign In', async () => {
-        await page.goto('https://www.saucedemo.com/')
+        await page.goto('data.url')
 
-        await page.fill('input[id="user-name"]', 'standard_user')
-        await page.fill('input[type="password"]', 'secret_sauce')
+        await page.fill('input[id="user-name"]', 'data.user.')
+        await page.fill('input[type="password"]', 'password')
 
         await Promise.all([
           page.waitForNavigation(), 
