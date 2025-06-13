95b3554e35997d42f29d1b5537c8deaa29af9f05
Cleibson Silva <cleibsonslima@gmail.com>
2025-06-13 11:43:19 -0300

    centralizing reusable variables within a single file.

--- Playwright-Test/Playwright_Jest/__test__/TC01.js
+++ Playwright-Test/Playwright_Jest/__test__/TC01.js
@@ -1,15 +1,15 @@
-data = require('../data/data.json');
+const data = require('../data/data.json');
 
 describe('Post', () => {
 	test('Sign In', async () => {
-        await page.goto('data.url')
+		await page.goto(data.url);
 
-        await page.fill('input[id="user-name"]', 'data.user.')
-        await page.fill('input[type="password"]', 'password')
+		await page.fill('input[id="user-name"]', data.user);
+		await page.fill('input[type="password"]', data.password);
 
-        await Promise.all([
-          page.waitForNavigation(), 
-          page.click('input[type="submit"]') 
-        ])
-    })
-})
+		await Promise.all([
+			page.waitForNavigation(), 
+			page.click('input[type="submit"]') 
+		]);
+	});
+});
