0db262609bb0bed63088bfba2d8102b527dbc1cb
Cleibson Silva <cleibsonslima@gmail.com>
2025-06-17 16:03:15 -0300

    Adding content from empty folder

---
+++ Playwright-Test/PlayWrightGitHubActions/tests/teste.spec.js
@@ -0,0 +1,25 @@
+const { chromium } = require('playwright');
+const { test, expect } = require('@playwright/test');
+
+test('basic test', async () => {
+    const browser = await chromium.launch({headless:true})
+    const context = await browser.newContext();
+    const page = await context.newPage()
+
+    await page.goto('https://www.saucedemo.com/')
+
+    await page.fill('input[id="user-name"]', 'standard_user')
+    await page.press('input[id="user-name"]', 'Tab')
+    await page.type('input[type = "password"]', 'secret_sauce')
+    await page.press('input[type="text"]', 'Enter');
+
+
+    const html = await page.innerHTML('html');
+    expect(html).toContain('shopping_cart_link');
+
+
+    // Screenshot
+    await page.screenshot({path: 'SignIng.png', fullPage: true})
+
+    await browser.close()
+})
